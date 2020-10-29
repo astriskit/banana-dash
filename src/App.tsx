dbe8fbdeb2865d9cef16e81a54cb3f5797b39bfb
astriskit <harry2office@gmail.com>
2020-10-29 08:33:18 +0530

    app - base setup using CRA

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app" data-testid="app">
+      Hello banana!
+    </div>
+  );
+}
+
+export default App;
